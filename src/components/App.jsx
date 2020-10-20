ee62c3e6d10c68e993b8c3ca20928f01d5a10339
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-20 08:49:13 -0500

    Updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState("TIME");
+
+  function updateTime() {
+    const time = new Date().toLocaleTimeString();
+    setTime(time);
+    setInterval(updateTime, 1000);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
